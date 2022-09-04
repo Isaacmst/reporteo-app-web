974552ef05ecadf8d8ea50c8ada28a12425f00d6
Isaac <sacedotorres@gmail.com>
2022-09-03 21:55:05 -0500

    reporteo commit

---
+++ src/components/title.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Title (){
+    return (
+        <h1 className="title">
+            Kelly Services Reporteo{" "}
+            <span></span>
+                </h1>
+    );
+}
+
+export default Title;
+
+
